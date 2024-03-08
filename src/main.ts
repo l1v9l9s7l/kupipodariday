d5a6a9729ef914eeab7df8d00e042fea1fd47bb5
l1v9l9s7l <l1v9l9s7l@gmail.com>
2024-03-08 19:05:15 +0300

    1.0

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import "reflect-metadata";
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//     res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+//     next();
+//   });
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+import "reflect-metadata";
+import { HttpAdapterHost, NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import helmet from "helmet";
+import { AllExceptionsFilter } from "./exceptions/all-exceptions.filter";
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const httpAdapter = app.get(HttpAdapterHost);
+
+  app.use(
+    helmet()
+    // { crossOriginResourcePolicy: { policy: "cross-origin" } }
+  );
+
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(3000);
 }
 bootstrap();
+
+
+
