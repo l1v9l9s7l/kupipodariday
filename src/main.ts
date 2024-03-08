dea179500c81d352cd634904275cef5521035340
l1v9l9s7l <l1v9l9s7l@gmail.com>
2024-03-08 23:26:14 +0300

    1.1

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,3 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import "reflect-metadata";
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-//     res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
-//     next();
-//   });
-//   await app.listen(3000);
-// }
-// bootstrap();
-
 import "reflect-metadata";
 import { HttpAdapterHost, NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
